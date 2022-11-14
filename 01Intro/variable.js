456fa4f76424499425536d4e835741cbc3ae6e9d
basavarajakj2000 <basavarajakj06@gmail.com>
2022-11-14 13:15:18 +0530

    variable is completed

--- 01Intro/variable.js
+++ 01Intro/variable.js
@@ -1,2 +1,12 @@
+"use strict";
+exports.__esModule = true;
 var greetings = 'Hello Basavaraja';
 console.log(greetings);
+// greetings = 6 Error: Type 'number' is not assignable to type 'string'.
+// Number
+var userId = 334544;
+var myNum = 14;
+myNum.valueOf();
+userId.toFixed();
+// boolean
+var isLoggedIn = false;
--- 01Intro/variable.ts
+++ 01Intro/variable.ts
@@ -1,7 +1,24 @@
 let greetings: string = 'Hello Basavaraja';
 console.log(greetings);
-
 // greetings = 6 Error: Type 'number' is not assignable to type 'string'.
+
+// Number
+let userId: number = 334544;
 let myNum: number = 14;
 myNum.valueOf();
+userId.toFixed();
+
+// boolean
+let isLoggedIn: boolean = false;
+
+// Any - can use when dont want to particular value to cause type error
+
+let varAny: any = 1432;
+varAny = 'string';
+
+
+
+
+
+
 export {};
